f455693301237b451c89983d10ea168a2fe987a8
stefi23 <stefania.rosca2309@gmail.com>
2020-12-25 14:09:01 +0100

    added the with JS spans on the lables, updated style with transitions, update to html

---
+++ form-input-wave/script.js
@@ -0,0 +1,10 @@
+const labels = document.querySelectorAll('.form-control label');
+
+
+labels.forEach(label => {
+    console.log(label)
+    label.innerHTML = label.innerText
+    .split('')
+    .map((letter, index) => `<span style="transition-delay:${index * 100}ms">${letter}</span>`)
+    .join('')
+})

20e661dc9ec70c3b5b687a6a483ecbe75e307d24
stefi23 <stefania.rosca2309@gmail.com>
2020-12-26 17:36:20 +0100

    removed a console.log

--- form-input-wave/script.js
+++ form-input-wave/script.js
@@ -2,7 +2,6 @@ const labels = document.querySelectorAll('.form-control label');
 
 
 labels.forEach(label => {
-    console.log(label)
     label.innerHTML = label.innerText
     .split('')
     .map((letter, index) => `<span style="transition-delay:${index * 100}ms">${letter}</span>`)
